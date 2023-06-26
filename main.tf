74c56314e0bfa4941cf6fdfddb144b67c94682fa
Thunsuda Jaidee <103437681+minbams@users.noreply.github.com>
2023-06-26 09:30:33 +0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "azurerm" {
+    version = "2.36.0"
+    features {
+      
+    }
+}
+
+resource "azurerm_resource_group" "rg" {
+    name = "MyFristResourceGroup" 
+    location = "westeurope"
+}
